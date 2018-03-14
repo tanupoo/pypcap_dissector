230bbc41ca88cf48f3e7278611e8bd63d33f2fb6
Shoichi Sakane <github-shoichi@tanu.org>
2018-03-14 11:26:30 +0900

    supported ICMPV6 Echo Request/Reply

--- dissector_icmpv6.py
+++ dissector_icmpv6.py
@@ -19,6 +19,13 @@ def dissect_icmpv6(x):
         this[JK_EMSG] = emsg
         return this
 
+    # ICMPv6 Echo Request/Reply
+    if fld[JK_ICMPV6_TYPE] == 128:
+        fld[JK_ICMPV6_IDENT] = struct.unpack(">H", x[offset:offset+2])[0]
+        offset += 2
+        fld[JK_ICMPV6_SEQNO] = struct.unpack(">H", x[offset:offset+2])[0]
+        offset += 2
+
     if len(x[offset:]) > 0:
         fld[JK_PAYLOAD] = x[offset:]
 
